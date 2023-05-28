71b39c76169e39c0a2366129aab0295f63f56225
Mr_Mathematica <114607917+conley024@users.noreply.github.com>
2023-05-28 00:01:22 -0400

    Add files via upload

---
+++ ShowOthersCoding/showLayout.java
@@ -0,0 +1,51 @@
+public class showLayout {
+    public static void main(String[] args){
+// Syntax in java can be easy to read at times //
+// Use "//" to insert a comment line//
+// You must also close the comment line with a //
+
+//Public static void main String args is passing a string through the main void that users see. //
+//These are UNDCELARED variables//
+int x;
+float y;
+double z;
+boolean logos;
+String words;
+
+// You can initialize variables from integers "int", decimals"float,double ", letters, or boolean values.//
+//you have to end statements with a semi colon ";"//
+//You can also choose to let these values equal something in the beginning//
+int ex1 = 3;
+double ex3=2.7534444;
+boolean exboolean;
+String examplename = "Example Message";
+
+
+//Use System.out.println to print a statement. Strings are supposed to have "" around them
+System.out.println("Hello World!");
+
+//This prints out the string
+System.out.println(examplename);
+
+//This prints out the double//
+System.out.println(ex3);
+
+//Lets do some math with the numbers above//
+//this is addition//
+System.out.println(ex1+ex3);
+//this is subtraction//
+System.out.println(ex1-ex3);
+//This is multiplication//
+System.out.println(ex1*ex3);
+// Division is a little different//
+
+System.out.println(ex1%ex3);
+
+
+System.out.println(ex1/ex3);
+
+
+//What were the undeclared variables for? to get user input//
+
+    }
+}
